473c8fba88fe3ff9943fabc3e92d9f86516f209e
Robert Sahm <robertsahm@gmail.com>
2012-05-02 18:51:13 -0400

    start working on jquery content switching

--- js/script.js
+++ js/script.js
@@ -1,8 +1,44 @@
-/* Author:
+//
+// Gentlemen, start your engines...
+//
+var main = $('#main')
+	// Navigation click variables
+	var about = $('#about a');
+	var portfolio = $('#portfolio a');
+	var clientList = $('#clientList a');
+	var portfolioDropdown = $('.portfolioDropdown');
 
-*/
+//
+// 3...2...1... G O ! ! !
+//
+
+$(function() { 
+// Load home page image on document load 
+	$(main).load('home.html');
+	//$('div#homeImageWrapper').addClass("current");
+// create clickable elements
+	// about click functions
+	$(about).click(function() {
+		
+		$('.current').fadeOut( 500 , function() {
+			// fadeOut Complete
+			$(main).load('about.html').hide().fadeIn(500);
+			// hide => fadeIn complete
+		});		
+	});
+
+	// Portfolio click functions
+	$(portfolio).click(function() {
+		$(portfolioDropdown).slideToggle(600);
+		$(main).load('portfolio.html').hide().fadeIn(500);
+	});
+
+	$(clientList).click(function() {
+		
+	});
 
 
 
 
 
+});  
