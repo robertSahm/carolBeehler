015e3113046b5c4f6da0ac679aaf92f418fb8613
Robert Sahm <robertsahm@gmail.com>
2012-05-05 01:38:39 -0400

    finished up day four, beginning custom switcher

--- js/script.js
+++ js/script.js
@@ -1,42 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// Gentlemen, start your engines...
+//
+//3... 2.... 1.... BAP!
 
 $(function() { 
 // Load home page image on document load 
 	$('#main').load('home.html').hide().fadeIn(700);
+	$()
 
 	// fades out top-right address on all nav clicks
 	$('.navText a').click(function() {
@@ -45,15 +15,122 @@ $(function() {
 
 	//fades out visible divs in #main
 	$('a.headerClick').click(function() {
+		$('li#portfolioDropdownLi').slideUp(400);
+		$('.navClick').css({
+				'font-weight': '400',
+				'color':'#111'
+    		}, 700);
 		$('#main').load('home.html').hide().fadeIn(700);
 		$('.topAddress').delay(700).fadeIn(700);
 	});
 
 	// About Click Stuff
-	$('a#about a').click()
+	$('a.aboutA').click(function() {
+		$('#main').load('about.html').hide().fadeIn(700);
+		// Closes portfolio dropdown if open
+		$('li#portfolioDropdownLi').slideUp(400);
+		$(this).css({
+			'font-weight':'800',
+			'color' : '#EE3124'
+		}).addClass('clicked');
+
+		if($(this).css('font-weight') === '800') {
+			$('.navClick:not(a.aboutA)').css({
+				'font-weight': '400',
+				'color':'#111'
+    		}, 700);
+		};
+	});
+
+	// Portfolio Click Stuff
+	$('a.portfolioA').click(function() {
+		$('#main').load('portfolio.html').hide().fadeIn(700);
+		$('li#portfolioDropdownLi').slideDown(400);
+		$(this).css({
+			'font-weight':'800',
+			'color' : '#EE3124'
+		}).addClass('clicked');
+
+		if($(this).css('font-weight') === '800') {
+			$('.navClick:not(a.portfolioA)').css({
+				'font-weight': '400',
+				'color':'#111'
+    		}, 700);
+		};
+	});
+
+	// Books Click Stuff
+	$('a.booksA').click(function() {
+		$('#main').load('books.html').hide().fadeIn(700);
+		$(this).css({
+			'font-weight':'800',
+			'color' : '#A484BB'
+		}).addClass('clicked');
+
+		if($(this).css('font-weight') === '800') {
+			$('.navClick:not(a.booksA)').css({
+				'font-weight': '400',
+				'color':'#111'
+    		});
+		};
+	});
+
+	// Brochures Click Stuff
+	$('a.brochuresA').click(function() {
+		$('#main').load('brochures.html').hide().fadeIn(700);
+		$(this).css({
+			'font-weight':'800',
+			'color' : '#F0902F'
+		}).addClass('clicked');
+
+		if($(this).css('font-weight') === '800') {
+			$('.navClick:not(a.brochuresA)').css({
+				'font-weight': '400',
+				'color':'#111'
+    		});
+		};
+	});
+
+	// Ephemera Click Stuff
+	$('a.ephemeraA').click(function() {
+		$('#main').load('ephemera.html').hide().fadeIn(700);
+		$(this).css({
+			'font-weight':'800',
+			'color' : '#8AC749'
+		}).addClass('clicked');
+
+		if($(this).css('font-weight') === '800') {
+			$('.navClick:not(a.ephemeraA)').css({
+				'font-weight': '400',
+				'color':'#111'
+    		});
+		};
+	});
+
+	// clientList Click Stuff
+	$('a.clientListA').click(function() {
+		$('#main').load('clientList.html').hide().fadeIn(700);
+		// Closes portfolio dropdown if open
+		$('li#portfolioDropdownLi').slideUp(400);
+		$(this).css({
+			'font-weight':'800',
+			'color' : '#EE3124'
+		}).addClass('clicked');
+
+		if($(this).css('font-weight') === '800') {
+			$('.navClick:not(a.clientListA)').css({
+				'font-weight': '400',
+				'color':'#111'
+    		});
+		};
+	});
+}); // End Nav clickiness
+
+$(function() { 
+	$('a.booksClick1').click(function() {
+		$('#booksSlideshowWrapper').load('sliders/bookSlider1.html')
+	});
 });
-
-
 
 
 
